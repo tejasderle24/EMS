92e27f6bc3b1c23856b2d990fa4f536a7504ff6e
tejasderle24 <tejasderle24@gmail.com>
2025-01-28 17:26:18 +0530

    UI Design Complated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { useEffect,useState } from 'react'
 import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashbord/EmployeeDashboard'
+import AdminDashboard from './components/Dashbord/AdminDashboard'
+import { getLocalStorage, setLocalStorage } from './utils/LocalStorage';
 
 function App() {
+
+  const [user, setUser] = useState("");
+
+  const handleLogin = () => {
+    
+  }
+
+
+  useEffect(() => {
+    // setLocalStorage()
+    getLocalStorage()
+  }, []);
+
+
   return (
     <>
-      <Login />
+      {!user ? <Login /> : ''}
+      {/* <EmployeeDashboard /> */}
+      {/* <AdminDashboard /> */}
     </>
   )
 }
